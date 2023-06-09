0ed22f2f178b42789b440752abc50e97894029ea
Serious-Fin <augustasgri@gmail.com>
2023-06-09 13:59:18 +0300

    Add references to departure and arrival airports for Flight model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_103342) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_105007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_103342) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "departure_airport_id"
+    t.bigint "arrival_airport_id"
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
