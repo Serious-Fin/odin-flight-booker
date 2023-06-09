807eb8425c273c4e4117ec6c93059c3e51047ee0
Serious-Fin <augustasgri@gmail.com>
2023-06-09 14:23:35 +0300

    Fix seed file syntax error

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,16 +21,16 @@ airports = Airport.create([
 ])
 
 flights = Flight.create([
-  { departure_airport_id: 1, arrival_airport_id: 2, departure_time: DateTime.new(2023, 06, 10, 08, 22, 00), duration: "2:30:00"},
+  { departure_airport_id: 1, arrival_airport_id: 2, departure_time: DateTime.new(2023, 06, 10, 8, 22, 00), duration: "2:30:00"},
   { departure_airport_id: 9, arrival_airport_id: 7, departure_time: DateTime.new(2023, 06, 11, 22, 34, 00), duration: "3:30:00"},
   { departure_airport_id: 7, arrival_airport_id: 2, departure_time: DateTime.new(2023, 07, 15, 14, 41, 00), duration: "2:14:00"},
   { departure_airport_id: 2, arrival_airport_id: 3, departure_time: DateTime.new(2023, 06, 11, 23, 00, 00), duration: "1:40:00"},
-  { departure_airport_id: 1, arrival_airport_id: 5, departure_time: DateTime.new(2023, 06, 20, 09, 16, 00), duration: "1:50:00"},
+  { departure_airport_id: 1, arrival_airport_id: 5, departure_time: DateTime.new(2023, 06, 20, 9, 16, 00), duration: "1:50:00"},
   { departure_airport_id: 9, arrival_airport_id: 6, departure_time: DateTime.new(2023, 07, 01, 11, 25, 00), duration: "3:15:00"},
   { departure_airport_id: 6, arrival_airport_id: 4, departure_time: DateTime.new(2023, 06, 17, 10, 30, 00), duration: "1:10:00"},
-  { departure_airport_id: 3, arrival_airport_id: 7, departure_time: DateTime.new(2023, 07, 02, 06, 40, 00), duration: "1:20:00"},
+  { departure_airport_id: 3, arrival_airport_id: 7, departure_time: DateTime.new(2023, 07, 02, 6, 40, 00), duration: "1:20:00"},
   { departure_airport_id: 1, arrival_airport_id: 10, departure_time: DateTime.new(2023, 06, 12, 16, 51, 00), duration: "8:40:00"},
   { departure_airport_id: 7, arrival_airport_id: 8, departure_time: DateTime.new(2023, 06, 23, 18, 32, 00), duration: "6:26:00"},
   { departure_airport_id: 5, arrival_airport_id: 4, departure_time: DateTime.new(2023, 07, 05, 18, 18, 00), duration: "1:20:00"},
-  { departure_airport_id: 6, arrival_airport_id: 1, departure_time: DateTime.new(2023, 06, 14, 08, 09, 00), duration: "3:05:00"}
+  { departure_airport_id: 6, arrival_airport_id: 1, departure_time: DateTime.new(2023, 06, 14, 8, 9, 00), duration: "3:05:00"}
 ])
